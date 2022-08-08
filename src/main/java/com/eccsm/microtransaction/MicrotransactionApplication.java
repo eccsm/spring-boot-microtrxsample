2fa2286b166cce44e80d641dc66f219c22703121
Casim <Ekincan.Casim@bs.nttdata.com>
2022-08-08 14:53:06 +0300

    starter fixed

--- src/main/java/com/eccsm/microtransaction/MicrotransactionApplication.java
+++ src/main/java/com/eccsm/microtransaction/MicrotransactionApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application-${spring.profiles.active:defdault}.properties")
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class MicrotransactionApplication {
 
 	public static void main(String[] args) {
